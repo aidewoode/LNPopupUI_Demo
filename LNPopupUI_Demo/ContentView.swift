bb640f3ff34ca61a6a802040fd40fcc78207acd3
aidewoode <aidewoode@gmail.com>
2022-12-09 09:57:16 +0800

    Add demo

--- LNPopupUI_Demo/ContentView.swift
+++ LNPopupUI_Demo/ContentView.swift
@@ -6,21 +6,50 @@
 //
 
 import SwiftUI
+import LNPopupUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+  @State var isLoggedIn = false
+  @State var isPlayerPresented = true
+  @State var emailField = ""
+
+  var body: some View {
+    if isLoggedIn {
+      VStack {
+        Image(systemName: "globe")
+          .imageScale(.large)
+          .foregroundColor(.accentColor)
+        Text("Hello, world!")
+        Button(action: {
+          isLoggedIn = false
+        }, label: {
+          Text("Logout")
+        })
+      }
+      .popup(isBarPresented: $isPlayerPresented, popupContent: {
+        Text("Player Content")
+      })
+      .popupBarCustomView {
+        HStack {
+          Text("Song Title")
         }
         .padding()
+      }
+    } else {
+      Form {
+        TextField("email", text: $emailField)
+        Button(action: {
+          isLoggedIn = true
+        }, label: {
+          Text("Login")
+        })
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
