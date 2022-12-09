fb71ed5959eeb84149322d18ba0b2d6c5dd34b3c
aidewoode <aidewoode@gmail.com>
2022-12-09 09:22:32 +0800

    Initial Commit

---
+++ LNPopupUI_Demo/LNPopupUI_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LNPopupUI_DemoApp.swift
+//  LNPopupUI_Demo
+//
+//  Created by Ed on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct LNPopupUI_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
